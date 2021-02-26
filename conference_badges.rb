66256fb2a8e181ef46f8df413b6711670dd2808e
burkemm <burkemm84@gmail.com>
2021-02-25 23:21:07 -0600

    commit tree 982728e8fa5cc29bb56b8bbecf8301ef1b63f358

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(people)
+    people.map do | greeting |
+    "Hello, my name is #{greeting}."
+    end
+end
+
+def assign_rooms(people)
+    people.map.with_index(1) do | speaker, home |
+    "Hello, #{speaker}! You'll be assigned to room #{home}!"
+    end
+end
+
+def printer(people)
+    batch_badge_creator(people).each do | badge |
+    puts badge
+    end
+
+    assign_rooms(people).each do | home |
+    puts home
+    end
+end
